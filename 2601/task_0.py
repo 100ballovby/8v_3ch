dd7ed006720f22137a2080bfefdee5e668a2f4a4
demidraksin <greatraksin@icloud.com>
2021-01-26 12:20:24 +0300

    Adding tasks with solution✨

---
+++ 2601/task_0.py
@@ -0,0 +1,12 @@
+'''Task 0:
+Используя цикл while, выведите на
+экран для числа 2 его степени от 0 до 20.
+Возведение в степень - **
+'''
+power = 0
+while power < 21:
+    print(2 ** power)
+    power += 1
+
+for pow in range(21):
+    print(2 ** pow)
